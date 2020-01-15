5a09ba615957ad7188ee50b55f7a5f783d2b351e
Yousef <yoyosanea@gmail.com>
2020-01-14 22:34:54 -0700

    Added A* Pathfinding

---
+++ Source/Mygame1/Assets/Scripts/EnemyGFX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyGFX : MonoBehaviour{
+private SpriteRenderer spriteRender;
+    // Start is called before the first frame update
+ public AIPath aiPath;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(aiPath.desiredVelocity.x >= 0.1f){
+			transform.localScale = new Vector3(-1f,1f,1f);
+		}
+		else if (aiPath.desiredVelocity.x <= -0.1f){
+			transform.localScale = new Vector3(1f,1f,1f);
+		}
+			
+    }
+}
